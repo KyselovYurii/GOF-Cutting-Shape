e1de44d05ea76bd9663d068a87dd791505a141f1
KyselovYurii <kyselov.yurii@gmail.com>
2022-01-31 12:16:37 +0200

    Adapter: Task1

---
+++ Adapter/Adapter.Task1/Container.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Adapter.Task1
+{
+    public class Container<T> : IContainer<T>
+    {
+        public Container(IEnumerable<T> items)
+        {
+            Items = items;
+        }
+
+        public IEnumerable<T> Items { get; }
+
+        public int Count => Items.Count();
+    }
+}
---
+++ Adapter/Adapter.Task1/MyPrinterAdapter.cs
@@ -0,0 +1,17 @@
+﻿namespace Adapter.Task1
+{
+    internal class MyPrinterAdapter : IMyPrinter
+    {
+        private readonly Printer _printer;
+
+        public MyPrinterAdapter(Printer printer)
+        {
+            _printer = printer;
+        }
+
+        public void Print<T>(IElements<T> elements)
+        {
+            _printer.Print(new Container<T>(elements.GetElements()));
+        }
+    }
+}
