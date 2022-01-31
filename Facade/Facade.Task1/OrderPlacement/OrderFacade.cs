4dc7734afa50fbf4d9f2741437ccfeeea84a9804
KyselovYurii <kyselov.yurii@gmail.com>
2022-01-31 13:11:26 +0200

    Facade: Task1

--- Facade/Facade.Task1/OrderPlacement/OrderFacade.cs
+++ Facade/Facade.Task1/OrderPlacement/OrderFacade.cs
@@ -4,9 +4,37 @@ namespace Facade.Task1.OrderPlacement
 {
     public class OrderFacade
     {
-        public void PlaceOrder(string productId, int quantity, string email)
+        private InvoiceSystem _invoiceSystem;
+        private PaymentSystem _paymentSystem;
+        private ProductCatalog _productCatalog;
+
+        public OrderFacade(InvoiceSystem invoiceSystem, PaymentSystem paymentSystem, ProductCatalog productCatalog)
         {
-            throw new Exception();
+            _invoiceSystem = invoiceSystem;
+            _paymentSystem = paymentSystem;
+            _productCatalog = productCatalog;
         }
+
+        public void PlaceOrder(string productId, int quantity, string email)
+		{
+			var product = _productCatalog.GetProductDetails(productId);
+
+			_paymentSystem.MakePayment(new Payment
+			{
+				ProductId = productId,
+				ProductName = product.Name,
+				Quantity = quantity,
+				TotalPrice = product.Price
+			});
+
+			_invoiceSystem.SendInvoice(new Invoice
+			{
+				ProductId = productId,
+				ProductName = product.Name,
+				Quantity = quantity,
+				TotalPrice = product.Price,
+				CustomerEmail = email
+			});
+		}
     }
 }
